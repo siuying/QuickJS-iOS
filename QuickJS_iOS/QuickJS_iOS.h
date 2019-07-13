fe46572b76a26b7fa6293af04e5175285b390849
Francis Chong <siu.ying@gmail.com>
2019-07-13 16:15:09 +0800

    Initial Commit

---
+++ QuickJS_iOS/QuickJS_iOS.h
@@ -0,0 +1,19 @@
+//
+//  QuickJS_iOS.h
+//  QuickJS_iOS
+//
+//  Created by Francis Chong on 13/7/2019.
+//  Copyright © 2019 Francis Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickJS_iOS.
+FOUNDATION_EXPORT double QuickJS_iOSVersionNumber;
+
+//! Project version string for QuickJS_iOS.
+FOUNDATION_EXPORT const unsigned char QuickJS_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickJS_iOS/PublicHeader.h>
+
+
---
+++ QuickJS_iOSTests/QuickJS_iOSTests.m
@@ -0,0 +1,37 @@
+//
+//  QuickJS_iOSTests.m
+//  QuickJS_iOSTests
+//
+//  Created by Francis Chong on 13/7/2019.
+//  Copyright © 2019 Francis Chong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuickJS_iOSTests : XCTestCase
+
+@end
+
+@implementation QuickJS_iOSTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
